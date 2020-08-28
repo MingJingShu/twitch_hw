96d4a5accb40ca3de5a4e5a887f8c8f8ce96c85b
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-08-28 17:36:32 +0800

    update for twitch
    css跟框架

--- js/index.js
+++ js/index.js
@@ -6,18 +6,7 @@ $(document).ready(function() {
 
 
 
-var swiper = new Swiper('.swiper-container', {
-  loop : true,
-  autoplay: {
-    delay: 3000,
-    stopOnLastSlide: false,
-    disableOnInteraction: true,
-    },
-  navigation: {
-    nextEl: '.R_btn',
-    prevEl: '.L_btn',
-  }
-});
+
 
 
 
